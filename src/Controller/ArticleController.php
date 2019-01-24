1cc7a3f9784d44c0b19d916871da7ac5e83593ef
Jacek Boguta <jacekboguta@gmail.com>
2019-01-24 16:00:04 +0100

    New article working

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,23 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class ArticleController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="article_list")
      * @Method({"GET"})
      */
     public function index()
     {
 
-        $articles = ['Article 1', 'Article 2'];
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
-        return $this->render('articles\index.html.twig', array('articles' => $articles));
-
+        return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
+
+    /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array('attr' => array(
+                'class' => 'form-control')))
+            ->add('body', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        return $this->render('articles/new.html.twig', array('form' => $form->createView()));
+    }
+
+
 }
+
